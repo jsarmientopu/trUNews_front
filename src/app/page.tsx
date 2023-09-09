09c0a43289b39ea740597eaabcf0ef6ab147a94e
dsolisav <dsolis@unal.edu.co>
2023-09-09 17:05:33 -0500

    update links in navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+'use client'
 import Image from 'next/image'
 import LoadingButton from '@/components/LoadingButton';
+import {Button} from '@nextui-org/react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <LoadingButton>Hola Mundo</LoadingButton>
+      <Button>
+        <Link href="/crear-articulo">Crear artículo</Link>
+      </Button>
     </main>
   )
 }
