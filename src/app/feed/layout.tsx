9b1355bb82070fa08154173d5b0d2334842e4ad7
Warspyt <tatianax2442@gmail.com>
2023-10-07 02:07:13 -0500

    fetch de todos los articulos en la vista del feed

--- src/app/feed/layout.tsx
+++ src/app/feed/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout ({children}:{
   children: React.ReactNode
 }){
     return (
-        <div className='bg-[#C1D6E8]'>
+        <div className='h-full bg-[#C1D6E8]'>
             {children}
         </div>
     )
--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -2,40 +2,76 @@
 
 import React, {useState, useEffect} from "react";
 import FeedArticleCard from "@/components/feed/FeedArticleCard";
+import { decryptedJWT } from "@/dto/users";
+import { getFeed } from "@/utils/fetchs";
+import { getFromLocalStorage } from "@/utils/localStorage";
+import verifyToken from "@/utils/utils";
+import { redirect } from "next/navigation";
 
-export default function feedPage() {
+export default function FeedPage() {
+
+    const [userInfo, setUserInfo] = useState<decryptedJWT>({userId:-2,rol:-1})
+    const [articlesFeed, setArticlesFeed] = useState<Array<any>>([{
+        'date':'',
+        'id_article': 0,
+        'id_writer': 0,
+        'image_url': '',
+        'lastname': '',
+        'name': '',
+        'sanitizedText': '',
+        'text': '',
+        'title': '',
+        'username': '',
+        'views': 0
+    }]);
+
+    async function token(){
+        const tok = getFromLocalStorage("token");
+        if(tok){
+            const rol =await verifyToken();
+            setUserInfo(rol);
+        }else{
+            setUserInfo({userId:-1,rol:-1});
+        }
+    }
+
+    useEffect(()=>{
+        token()
+    },[])
+
+    useEffect(() => {
+        async function fetchData() {
+            const feedData = await getFeed();
+            setArticlesFeed(feedData);
+        }
+
+        fetchData();
+  }, []);
+
+    if(userInfo.userId==-2) return
+    if(userInfo.userId==-1) redirect('/');
+
     return (
         <div className="py-2">
-            <div className="flex justify-center py-unit-4">
-                <FeedArticleCard 
-                    imageArticle="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-                    profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
-                    autor="Tatiana Alvarez"
-                    username="Warpyt"
-                    title="Título del Artículo"
-                    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                />
-            </div>
-            <div className="flex justify-center py-unit-4">
-                <FeedArticleCard 
-                    imageArticle="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-                    profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
-                    autor="Tatiana Alvarez"
-                    username="Warpyt"
-                    title="Título del Artículo"
-                    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                />
-            </div>
-            <div className="flex justify-center py-unit-4">
-                <FeedArticleCard 
-                    imageArticle="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
-                    profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
-                    autor="Tatiana Alvarez"
-                    username="Warpyt"
-                    title="Título del Artículo"
-                    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                />
-            </div>
+            {articlesFeed.length!==0?
+                articlesFeed.map((item, index) => (
+                    <div className="flex justify-center py-unit-4" key={index}>
+                        <FeedArticleCard 
+                            imageArticle = {item.image_url}
+                            profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
+                            autor = {item.name}
+                            username = {item.username}
+                            title = {item.title}
+                            summary = {item.sanitizedText}
+                        />
+                    </div>
+                ))
+            :
+                <div className='text-center font-bold text-2xl p-5'>
+                    No hay articulos para ver
+                </div>
+
+            }
         </div>
     )
 }
