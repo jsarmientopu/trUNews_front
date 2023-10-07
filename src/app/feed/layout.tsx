6fc5618280e56c17ffa9865da02e604be34b0839
Warspyt <tatianax2442@gmail.com>
2023-10-06 22:34:59 -0500

    prueba vista

--- src/app/feed/layout.tsx
+++ src/app/feed/layout.tsx
@@ -2,7 +2,7 @@ export default function Layout ({children}:{
   children: React.ReactNode
 }){
     return (
-        <div>
+        <div className='bg-[#C1D6E8]'>
             {children}
         </div>
     )
--- src/app/feed/page.tsx
+++ src/app/feed/page.tsx
@@ -1,5 +1,41 @@
+'use client'
+
+import React, {useState, useEffect} from "react";
+import FeedArticleCard from "@/components/feed/FeedArticleCard";
+
 export default function feedPage() {
     return (
-        <h1> Prueba feed </h1>
+        <div className="py-2">
+            <div className="flex justify-center py-unit-4">
+                <FeedArticleCard 
+                    imageArticle="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                    profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
+                    autor="Tatiana Alvarez"
+                    username="Warpyt"
+                    title="Título del Artículo"
+                    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+            </div>
+            <div className="flex justify-center py-unit-4">
+                <FeedArticleCard 
+                    imageArticle="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                    profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
+                    autor="Tatiana Alvarez"
+                    username="Warpyt"
+                    title="Título del Artículo"
+                    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+            </div>
+            <div className="flex justify-center py-unit-4">
+                <FeedArticleCard 
+                    imageArticle="https://nextui-docs-v2.vercel.app/images/hero-card-complete.jpeg"
+                    profileImage="https://i.pravatar.cc/150?u=a04258114e29026702d"
+                    autor="Tatiana Alvarez"
+                    username="Warpyt"
+                    title="Título del Artículo"
+                    summary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+                />
+            </div>
+        </div>
     )
 }
