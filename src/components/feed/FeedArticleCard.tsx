ad1fa233ff7763af03efc82058091d4df1846f3c
Warspyt <tatianax2442@gmail.com>
2023-10-13 12:59:56 -0500

    cambios sugeridos

--- src/components/feed/FeedArticleCard.tsx
+++ src/components/feed/FeedArticleCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Card, Button, Image, User } from '@nextui-org/react';
+import { Card, CardBody, Image, User } from '@nextui-org/react';
+import '../../app/globals.css'
 
 interface FeedArticleCardProps {
   imageArticle: string;
@@ -12,6 +13,9 @@ interface FeedArticleCardProps {
   idWriter: number;
   views: number;
   date: string;
+  saved: boolean;
+  savedUsername: string;
+  categories: Array<string>;
 }
 
 const FeedArticleCard: React.FC<FeedArticleCardProps> = ({
@@ -24,43 +28,60 @@ const FeedArticleCard: React.FC<FeedArticleCardProps> = ({
   id,
   idWriter, 
   views,
-  date
+  date, 
+  saved, 
+  savedUsername,
+  categories
 }) => {
+  const redirectToArticle = () => {
+    window.location.href = `articulo/${id}`;
+  };
+
   return (
-    <Card className = "w-[80%] p-unit-lg shadow-lg bg-[#F0F2F4]">
-      <div className='sm:block md:flex items-center'>
-        <div className='flex-1'>
-          <div className='flex justify-center'>
-            <a href={`articulo/${id}`}>
+    <Card className = "article_card w-[80%] p-unit-lg shadow-lg bg-[#F0F2F4]" isPressable onPress={redirectToArticle} isHoverable>
+      <CardBody>
+          <div className='sm:block md:flex items-center'>
+          <div className='flex-1'>
+            <div className='flex justify-center'>
               <Image src={imageArticle} alt="Card Image" width={280} isZoomed style={{height: 180}}/>
-            </a>
-          </div> 
-          <div className='flex items-center justify-center py-4'>
-            <a className="pe-6 tap-highlight-transparent data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 hover:opacity-80 active:opacity-disabled transition-opacity" 
-              href={`perfil/?search=${idWriter}`}>
-              <User   
-                name={autor}
-                description={username}
-                avatarProps={{
-                  src: profileImage
-                }}
-              />
-            </a>
-            <p className='ps-3 text-sm'>{views} Views</p>
+            </div> 
+            <div className='flex items-center justify-center pt-4'>
+              <a className="pe-6 tap-highlight-transparent data-[focus-visible=true]:z-10 data-[focus-visible=true]:outline-2 data-[focus-visible=true]:outline-focus data-[focus-visible=true]:outline-offset-2 hover:opacity-80 active:opacity-disabled transition-opacity" 
+                href={`perfil/?search=${idWriter}`}>
+                <User   
+                  name={autor}
+                  description={username}
+                  avatarProps={{
+                    src: profileImage
+                  }}
+                />
+              </a>
+              <p className='ps-3 text-sm'>{views} Views</p>
+            </div>
+            {saved &&
+              <div className='flex justify-center py-4 text-sm'>
+                <p className='text-center'>Guardado por: {savedUsername}</p>
+              </div>
+            }
+          </div>
+          <div className='px-4' style={{ flex: 2 }}>
+            <p className='text-center font-bold text-3xl'>
+              {title}
+            </p>
+            <div className='py-unit-lg text-justify break-words'>
+              <p className='line-clamp-[4]'>{summary}</p>
+            </div>
+            <div className='pt-4 flex justify-end row gap-3 text-sm font-semibold text-zinc-600'>
+              {categories.map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </div>
+            <p className='pt-4 flex justify-end text-sm'>
+              Published: {date} 
+            </p>
           </div>
         </div>
-        <div className='px-4' style={{ flex: 2 }}>
-          <div className='text-center font-bold text-3xl'>
-            {title}
-          </div>
-          <div className='py-unit-lg text-justify break-words'>
-            <p className='line-clamp-[4]'>{summary}</p>
-          </div>
-          <div className='pt-4 flex justify-end text-sm'>
-            Published: {date}
-          </div>
-        </div>
-      </div>
+      </CardBody>
     </Card>
   );
 };
