212a38d38066e2921e6bbd97ee23c450ab2dbd9c
Warspyt <tatianax2442@gmail.com>
2023-10-13 23:02:18 -0500

    pagination

--- src/app/articles-by-category/[id]/page.tsx
+++ src/app/articles-by-category/[id]/page.tsx
@@ -3,6 +3,7 @@
 import React, {useState, useEffect} from "react";
 import ArticleCard from "@/components/articles-by-category/ArticleCard";
 import { getArticlesByCategory } from "@/utils/fetchs";
+import { Pagination } from '@nextui-org/react';
 
 export default function ArticlesByCategoryPage({ params }: any) {
 
@@ -22,12 +23,13 @@ export default function ArticlesByCategoryPage({ params }: any) {
         'article_has_categories': []
     }]);
 
-    // Rastreo de articulos visibles en el feed
-    const [visibleArticles, setVisibleArticles] = useState(1);
+    // Paginacion de articulos
+    const itemsPerPage = 5;
+    const [currentPage, setCurrentPage] = useState(1);
 
-    const handleVerMasClick = () => {
-        setVisibleArticles(prevVisibleArticles => prevVisibleArticles + 10);
-    }
+    const handlePageChange = (page: number) => {
+        setCurrentPage(page);
+    };
 
     // fetch de articulos a mostrar en el feed
     useEffect(() => {
@@ -35,7 +37,6 @@ export default function ArticlesByCategoryPage({ params }: any) {
             const articlesByCategoryData = await getArticlesByCategory(params.id);
             if(articlesByCategoryData){
                 setArticles(articlesByCategoryData);
-                setVisibleArticles(10);
             }
         }
 
@@ -44,9 +45,17 @@ export default function ArticlesByCategoryPage({ params }: any) {
     return (
         <div className="py-2">
             <p className="text-center font-bold text-3xl p-5">CATEGORY ARTICLES</p>
-            {articles.length !== 0 && articles.length > 1 ? (
+            <div className="hidden md:flex lg:flex justify-center py-unit-4">
+                <Pagination total={Math.ceil(articles.length / itemsPerPage)} page={currentPage} onChange={(page) => handlePageChange(page)} 
+                variant="light" size="lg" showControls siblings={2} radius="full"/>
+            </div>
+            <div className="flex md:hidden lg:hidden justify-center py-unit-4">
+                <Pagination total={Math.ceil(articles.length / itemsPerPage)} page={currentPage} onChange={(page) => handlePageChange(page)} 
+                variant="light" size="lg" showControls radius="full"/>
+            </div>
+            {articles.length !== 0 ? (
                 <>
-                {articles.slice(0, visibleArticles).map((item, index) => (
+                {articles.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage).map((item, index) => (
                     <div className="flex justify-center py-unit-4" key={index}>
                     <ArticleCard 
                         imageArticle={item.image_url}
@@ -63,25 +72,22 @@ export default function ArticlesByCategoryPage({ params }: any) {
                     />
                     </div>
                 ))}
-                {visibleArticles < articles.length && (
-                    <div className="flex justify-center py-unit-4">
-                    <button onClick={handleVerMasClick} className='bg-primary text-white py-2 px-3 rounded-xl'>
-                        See more
-                    </button>
-                    </div>
-                )}
-                {visibleArticles >= articles.length && (
-                    <div className='text-center font-bold text-2xl p-5'>
-                        There are no more articles to see
-                    </div>
-                )}
+                <div className="hidden md:flex lg:flex justify-center py-unit-4">
+                <Pagination total={Math.ceil(articles.length / itemsPerPage)} page={currentPage} onChange={(page) => handlePageChange(page)} 
+                    variant="light" size="lg" showControls siblings={2} radius="full"/>
+                </div>
+                <div className="flex md:hidden lg:hidden justify-center py-unit-4">
+                    <Pagination total={Math.ceil(articles.length / itemsPerPage)} page={currentPage} onChange={(page) => handlePageChange(page)} 
+                    variant="light" size="lg" showControls radius="full"/>
+                </div>
                 </>)
             :
                 <div className='h-screen bg-[#C1D6E8] text-center font-bold text-2xl p-5'>
                     Nothing to see
                 </div>
-
             }
         </div>
     )
 }
+
+
