5c86ae5d87809b83a7a27c1ca368d98c06f05b6c
jsarmientopu <jsarmientopu@unal.edu.co>
2023-10-17 19:47:22 -0500

    env

--- next.config.js
+++ next.config.js
@@ -4,11 +4,11 @@ const nextConfig = {
         domains:['picsum.photos',"loremflickr.com","trunews.s3.us-east-2.amazonaws.com"]
     },
     env: {
-        BACK_URL: 'http://localhost:3005/',
-        // BACK_URL: 'http://34.203.197.7:3005/', 
+        // BACK_URL: 'http://localhost:3005/',
+        BACK_URL: 'http://3.89.38.183:3005/', 
 
 
-        FRONT_URL: 'http://34.203.197.7/',
+        FRONT_URL: 'https://trunews-front-lac.vercel.app',
         // FRONT_URL: 'http://localhost:3000',
     },
     
